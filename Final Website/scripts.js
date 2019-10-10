df1393a019d3964e6ac0a204bed0c8ffba61207f
Owmacohe <44061383+Owmacohe@users.noreply.github.com>
2019-10-10 17:37:08 -0400

    Finsihed start page for final project

--- Final Website/scripts.js
+++ Final Website/scripts.js
@@ -1,21 +1,59 @@
-var slideFrame = -90;
+var slideFrame = -50;
 var slideMax = 0;
 
 var opFrame = 0;
 var opMax = 1;
 
-var counter = setInterval(slider, 20);
+var frameWait = 0;
+var waitMax = 2;
+var slideCounter = setInterval(slider, 10);
 
 function slider() {
-  if (slideFrame <= slideMax)
+  if (frameWait <= waitMax)
   {
-    slideFrame += 10;
-    document.querySelector(".flexenter").style.top = slideFrame;
+    frameWait += 0.05;
   }
+  else {
+    if (slideFrame <= slideMax)
+    {
+      slideFrame += (slideMax - slideFrame) / 20;
+      document.querySelector(".flexenter").style.top = slideFrame;
+    }
 
-  if (opFrame <= opMax)
-  {
-    opFrame += 0.02;
-    document.querySelector(".flexenter").style.opacity = opFrame;
+    if (opFrame <= opMax)
+    {
+      opFrame += opMax / 40;
+      document.querySelector(".flexenter").style.opacity = opFrame;
+    }
   }
+}
+
+var periodNum = 0;
+var enterCounter = setInterval(enterWait, 400);
+
+function enterWait() {
+  if (periodNum == 0)
+  {
+    periodNum++;
+    document.getElementById("enterID").innerHTML = "Enter";
+  }
+  else if (periodNum == 1)
+  {
+    periodNum++;
+    document.getElementById("enterID").innerHTML = "Enter.";
+  }
+  else if (periodNum == 2)
+  {
+    periodNum++;
+    document.getElementById("enterID").innerHTML = "Enter..";
+  }
+  else if (periodNum == 3)
+  {
+    periodNum = 0;
+    document.getElementById("enterID").innerHTML = "Enter...";
+  }
+}
+
+function split() {
+  console.log("Split window");
 }
