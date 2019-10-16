1f73e1979dd394eebc57c55d86f9b0473b810f21
Owen Hellum <omch20012@gmail.com>
2019-10-16 10:24:38 -0400

    coloured squares

--- Final Website/split-script.js
+++ Final Website/split-script.js
@@ -10,18 +10,33 @@ function split() {
     createDiv(divNum);
   }
 
-  console.log("Number of created divs: " + divNum);
+  console.log("Number of divs created: " + divNum);
 }
 
 function createDiv(instances) {
   var i;
   for (i = 0; i < instances; i++)
   {
-    var splitDiv = document.createElement("DIV");
-    document.getElementById("divs").appendChild(splitDiv);
-
-    splitDiv.className = "flex_split";
-
     divNum++;
+
+    var splitButt = document.createElement("BUTTON");
+    document.getElementById("divs").appendChild(splitButt);
+
+    //splitButt.onclick 
+
+    splitButt.className = "flex_split";
+    splitButt.style.backgroundColor = randColor();
   }
+}
+
+function randColor() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+
+  for (i = 0; i < 6; i++)
+  {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+
+  return color;
 }
