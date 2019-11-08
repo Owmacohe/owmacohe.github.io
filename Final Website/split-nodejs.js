37a7791d36a8a5f5d094bcd102057c4b37ba8565
Owmacohe <44061383+Owmacohe@users.noreply.github.com>
2019-11-08 17:09:29 -0500

    Rename

--- Final Website/split-nodejs.js
+++
@@ -1,6 +0,0 @@
-var fs = require('fs');
-
-fs.writeFile('/Users/ohellum/Documents/GitHub/owmacohe.github.io/Final\ Website/TestFile.txt', 'TEXT', function (err) {
-  if (err) throw err;
-  console.log('Saved!');
-});
---
+++ Final_Website/split-nodejs.js
@@ -0,0 +1,15 @@
+const http = require('http');const server = http.createServer((req, res) => {
+    res.end(`
+        <!doctype html>
+        <html>
+        <body>
+            <form action="/" method="post">
+                <input type="text" name="fname" /><br />
+                <input type="number" name="age" /><br />
+                <input type="file" name="photo" /><br />
+                <button>Save</button>
+            </form>
+        </body>
+        </html>
+    `);
+});server.listen(3000);
