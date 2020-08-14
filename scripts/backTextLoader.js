2e28708c1bea98f003a31650a305a522abf162db
OMCH <omch20012@gmail.com>
2020-08-14 16:55:54 -0400

    Added background text to most pages, changed minigame colour

---
+++ scripts/backTextLoader.js
@@ -0,0 +1,34 @@
+loadBackText();
+
+function loadBackText() {
+  var backTextElem = document.createElement("DIV");
+  document.body.appendChild(backTextElem);
+  backTextElem.setAttribute("id", "backgroundText");
+
+  backTextElem.innerHTML = getRandomCharacter(1550, "alphaNum");
+}
+
+function getRandomCharacter(iterations, type) {
+  var randOutput = "";
+  var characters;
+
+  if (type != null) {
+    if (type == "alpha") {
+      characters = "abcdefghijklmnopqrstuvwxyz";
+    }
+    else if (type == "alphaNum") {
+      characters = "abcdefghijklmnopqrstuvwxyz01234567";
+    }
+  }
+  else {
+    characters = "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-=_+`~[]{}:;,.?/";
+  }
+
+  //console.log(characters);
+
+  for (var i = 0; i < iterations; i++) {
+    randOutput += characters[Math.floor(Math.random() * characters.length)];
+  }
+
+  return randOutput;
+}
