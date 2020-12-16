8d367e40b44d9c25e6b4e70a7774a4c3aa5b41f6
Owmacohe <44061383+Owmacohe@users.noreply.github.com>
2020-12-16 14:07:38 -0500

    Started work on projects prompt page

---
+++ scripts/projects.js
@@ -0,0 +1,32 @@
+var projects = [
+  {
+    "desc": "programming a Javascript minigame",
+    "minHours": "1",
+    "maxHours": "2"
+  },
+  {
+    "desc": "programming a dumb little Unity RPG",
+    "minHours": "5",
+    "maxHours": "24"
+  },
+  {
+    "desc": "transcribing that old book you have",
+    "minHours": "1",
+    "maxHours": "3"
+  },
+  {
+    "desc": "learning the basics of ASL",
+    "minHours": "2",
+    "maxHours": "3"
+  },
+  {
+    "desc": "writing a wholly original song on guitar",
+    "minHours": "1",
+    "maxHours": "2"
+  },
+  {
+    "desc": "reading a super short book",
+    "minHours": "2",
+    "maxHours": "4"
+  }
+];
