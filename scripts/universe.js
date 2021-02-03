ecc5305f449fa458e65a1cb3a16aa12f18d44b33
OMCH <omch20012@gmail.com>
2021-02-03 17:26:49 -0500

    Updated universe page to have more detail, streamlined link find page

--- scripts/universe.js
+++ scripts/universe.js
@@ -1,11 +1,12 @@
 var order = [
-  ["galaxy cluster", 40, "spherical", "oblong", "irregular"],
-  ["galaxy", 20, "spiral", "barred spiral", "irregular", "lenticular"],
-  ["system cluster", 40, "spherical", "oblong", "irregular"],
-  ["solar system", 20, "single-starred", "multi-starred"],
-  ["planet", 10, "gas", "rocky", "liquid"],
+  ["galaxy cluster", 50, "spherical", "oblong", "irregular"],
+  ["galaxy", 25, "spiral", "barred spiral", "irregular", "lenticular"],
+  ["system cluster", 50, "spherical", "oblong", "irregular"],
+  ["solar system", 25, "single-starred", "multi-starred"],
+  ["planet", 10, "gas", "rocky", "liquid", "hybrid"],
   ["continent", 5, "northern", "equatorial", "southern"],
-  ["region", 5, "mountainous", "forested", "island", "swamp", "populated", "plains", "desert", "icy"]
+  ["region", 5, "mountainous", "forested", "island", "swamp", "populated", "plains", "desert", "icy"],
+  ["location", 3, "specific valley", "specific peak", "specific steppe", "specific building", "specific cave", "specific path", "specific slope", "specific field", "specific tree", "specific waterfall"]
 ];
 
 var currentLayerTab = 0;
@@ -29,18 +30,23 @@ function createTab(progenitor) {
 
   tabMargin = (thisTab[0] + 5) + "vw";
 
+  var newRow = document.createElement("DIV");
+  newRow.setAttribute("class", "flexrow");
+  newRow.style.marginLeft = tabMargin;
+  document.getElementById("tab_" + progenitor[0] + "_" + progenitor[1]).appendChild(newRow);
+
   var newButton = document.createElement("BUTTON");
   newButton.setAttribute("data-toggle", "collapse");
   newButton.setAttribute("data-target", "#tab_" + thisTab[0] + "_" + thisTab[1]);
-  newButton.style.marginLeft = tabMargin;
   newButton.innerHTML = order[progenitor[0]][Math.floor(Math.random() * (order[progenitor[0]].length - 2)) + 2] + " " + order[progenitor[0]][0];
-  document.getElementById("tab_" + progenitor[0] + "_" + progenitor[1]).appendChild(newButton);
+  newRow.appendChild(newButton);
+
+  newRow.innerHTML += "test";
 
   var newTab = document.createElement("DIV");
   newTab.setAttribute("id", "tab_" + thisTab[0] + "_" + thisTab[1]);
   newTab.setAttribute("class", "collapse");
   newTab.style.marginLeft = tabMargin;
-  newTab.innerHTML = "TestContent";
   document.getElementById("tab_" + progenitor[0] + "_" + progenitor[1]).appendChild(newTab);
 
   if (progenitor[0] != order.length - 1) {
