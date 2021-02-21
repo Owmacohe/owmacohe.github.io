23f8d3c9277f3cb92e4a5bd456a26ac6d2c9b849
OMCH <omch20012@gmail.com>
2021-02-21 12:06:14 -0500

    Fixed issues with universe page, small tweaks, re-added discord tag

--- scripts/backTextLoader.js
+++ scripts/backTextLoader.js
@@ -5,27 +5,29 @@ function loadBackText() {
   document.body.appendChild(backTextElem);
   backTextElem.setAttribute("id", "backgroundText");
 
-  //var charNum = (1440 / window.innerWidth) * 1440;
-  backTextElem.innerHTML = getRandomCharacter(2.5 * window.innerWidth, "alphaNum");
+  backTextElem.innerHTML = getRandomCharacter(2.5 * window.innerWidth, "special");
 }
 
 function getRandomCharacter(iterations, type) {
   var randOutput = "";
-  var characters;
+  var characters = "abcdefghijklmnopqrstuvwxyz";
 
   if (type != null) {
-    if (type == "alpha") {
-      characters = "abcdefghijklmnopqrstuvwxyz";
-    }
-    else if (type == "alphaNum") {
-      characters = "abcdefghijklmnopqrstuvwxyz01234567";
+    switch (type) {
+      case "alpha":
+        characters += "";
+        break;
+      case "alphaNum":
+        characters += "01234567";
+        break;
+      case "special":
+        characters += "0123456789`~!@#$;";
+        break;
     }
   }
   else {
-    characters = "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-=_+`~[]{}:;,.?/";
+    characters += "0123456789`~!@#$;";
   }
-
-  //console.log(characters);
 
   for (var i = 0; i < iterations; i++) {
     randOutput += characters[Math.floor(Math.random() * characters.length)];
--- scripts/universe.js
+++ scripts/universe.js
@@ -3,53 +3,66 @@ var order = [
   ["galaxy cluster", 50, "spherical", "oblong", "irregular"],
   ["galaxy", 25, "spiral", "barred spiral", "irregular", "lenticular"],
   ["system cluster", 50, "spherical", "oblong", "irregular"],
-  ["solar system", 25, "single-starred", "multi-starred"]/*,
-  ["planet", 10, "gas", "rocky", "liquid", "hybrid"]/*,
+  ["solar system", 25, "single-starred", "multi-starred"],
+  ["planet", 10, "gas", "rocky", "liquid", "hybrid"],
   ["continent", 5, "northern", "equatorial", "southern"],
   ["region", 5, "mountainous", "forested", "island", "swamp", "populated", "plains", "desert", "icy"],
-  ["location", 3, "specific valley", "specific peak", "specific steppe", "specific building", "specific cave", "specific path", "specific slope", "specific field", "specific tree", "specific waterfall"]*/
+  ["location", 3, "valley", "peak", "steppe", "building", "cave", "path", "slope", "field", "waterfall"]
 ];
-
-var tabs = [];
 
 var currentLayerTab = 0;
 
-createLayers([0, 0]);
+createLayer([0, 0]);
 
-function createLayers(progenitor) {
-  for (var i = 0; i < Math.floor(Math.random() * order[progenitor[0]][1]) + 1; i++) {
-    var temp = progenitor[0] + 1;
-    currentLayerTab++;
-    var thisTab = [temp, currentLayerTab];
-    var marg;
+function createLayer(progenitor) {
+  var prog = document.getElementById("tab_" + progenitor[0] + "_" + progenitor[1]);
 
-    if (thisTab[0] == 1) {
-      marg = 0;
-    }
-    else {
-       marg = (thisTab[0] + 2);
-    }
+  if (prog.hasChildNodes() == false || progenitor[0] == 0) {
+    for (var i = 0; i < Math.floor(Math.random() * order[progenitor[0]][1]) + 1; i++) {
+      var temp = progenitor[0] + 1;
+      currentLayerTab++;
+      var thisTab = [temp, currentLayerTab];
 
-    var newGroup = document.createElement("DIV");
-    newGroup.style.marginLeft = marg + "vw";
-    document.getElementById("tab_" + progenitor[0] + "_" + progenitor[1]).appendChild(newGroup);
-
-    var newButton = document.createElement("BUTTON");
-    newButton.setAttribute("class", "accordion");
-    newButton.innerHTML = order[progenitor[0]][Math.floor(Math.random() * (order[progenitor[0]].length - 2)) + 2] + " " + order[progenitor[0]][0];
-    newGroup.appendChild(newButton);
-
-    var newTab = document.createElement("DIV");
-    newTab.setAttribute("class", "panel");
-    newTab.setAttribute("id", "tab_" + thisTab[0] + "_" + thisTab[1]);
-    newGroup.appendChild(newTab);
-
-    if (progenitor[0] != order.length - 1) {
-      createLayers(thisTab);
+      if (progenitor[0] != order.length) {
+        createTab(progenitor, thisTab);
+      }
     }
   }
 }
 
-function createDescription() {
-  //TODO
+function createTab(progenitor, thisTab) {
+  var marg;
+
+  if (thisTab[0] == 1) {
+    marg = 0;
+  }
+  else {
+     marg = (thisTab[0] + 2);
+  }
+
+  var newGroup = document.createElement("DIV");
+  newGroup.style.marginLeft = marg + "vw";
+  document.getElementById("tab_" + progenitor[0] + "_" + progenitor[1]).appendChild(newGroup);
+
+  var newButton = document.createElement("BUTTON");
+  newButton.setAttribute("class", "accordion");
+  newButton.setAttribute("onclick", "createLayer([" + thisTab[0] + ", " + thisTab[1] + "])");
+  newButton.innerHTML = order[progenitor[0]][Math.floor(Math.random() * (order[progenitor[0]].length - 2)) + 2] + " " + order[progenitor[0]][0];
+  newButton.addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    panel.style.transition = "2.5s";
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = (panel.scrollHeight * 2) + "vw";
+    }
+  });
+  newGroup.appendChild(newButton);
+
+  var newTab = document.createElement("DIV");
+  newTab.setAttribute("id", "tab_" + thisTab[0] + "_" + thisTab[1]);
+  newTab.style.backgroundColor = "hsla(0, 0%, 100%, 0.2)";
+  newTab.style.paddingRight = "1vw";
+  newGroup.appendChild(newTab);
 }
