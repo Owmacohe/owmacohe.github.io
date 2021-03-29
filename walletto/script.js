2ef16988c2d17382644714b7544b362f3cb597e0
Owmacohe <44061383+Owmacohe@users.noreply.github.com>
2021-03-29 19:18:57 -0400

    Finished Walletto character generator

--- walletto/script.js
+++ walletto/script.js
@@ -6,18 +6,70 @@ var inventories = ["Rope", "Book", "Drugs", "Trinket", "Costume", "Companion", "
 
 function generate() {
   var stats = [];
-  stats[0] = rollDie(20, true); //STRN
-  stats[1] = rollDie(20, true); //DEXT
-  stats[2] = rollDie(20, true); //INTL
-  stats[3] = rollDie(20, true); //MAGI
+  stats[0] = rollDie(20); //STRN
+  stats[1] = rollDie(20); //DEXT
+  stats[2] = rollDie(20); //INTL
+  stats[3] = rollDie(20); //MAGI
   stats[4] = stats[0]; //HLTH
   stats[5] = Math.ceil((stats[0] + stats[1]) / 2) //ARMR
 
   var myRace = rollArray(races);
   var myClass = rollArray(classes);
   var mySpells = "";
+  var spellsTemp = [];
   var myWeapons = "";
+  var weaponsTemp = [];
   var myInventory = "";
+  var inventoryTemp = [];
+
+  switch (myClass) {
+    case "Wizard":
+      for (var i = 0; i < 4; i++) {
+        spellsTemp[i] = rollArray(spells);
+        spells.splice(spells.indexOf(spellsTemp[i]), 1);
+        mySpells += spellsTemp[i] + "<br>";
+      }
+
+      for (var j = 0; j < 2; j++) {
+        weaponsTemp[i] = rollArray(weapons);
+        weapons.splice(weapons.indexOf(weaponsTemp[i]), 1);
+        myWeapons += weaponsTemp[i] + "<br>";
+      }
+      break;
+    case "Fighter":
+      stats[0] += " +3";
+      stats[1] += " +3";
+
+      mySpells = "No spells";
+
+      for (var j = 0; j < 2; j++) {
+        weaponsTemp[i] = rollArray(weapons);
+        weapons.splice(weapons.indexOf(weaponsTemp[i]), 1);
+        myWeapons += weaponsTemp[i] + " +2<br>";
+      }
+      break;
+    case "War Mage":
+      stats[0] += " +2";
+
+      for (var i = 0; i < 2; i++) {
+        spellsTemp[i] = rollArray(spells);
+        spells.splice(spells.indexOf(spellsTemp[i]), 1);
+        mySpells += spellsTemp[i] + "<br>";
+      }
+
+      for (var j = 0; j < 2; j++) {
+        weaponsTemp[i] = rollArray(weapons);
+        weapons.splice(weapons.indexOf(weaponsTemp[i]), 1);
+        myWeapons += weaponsTemp[i] + " +1<br>";
+      }
+      break;
+  }
+
+  for (var k = 0; k < 4; k++) {
+    inventoryTemp[i] = rollArray(inventories);
+    inventories.splice(inventories.indexOf(inventoryTemp[i]), 1);
+    myInventory += inventoryTemp[i] + "<br>";
+  }
 
   document.getElementById("r").innerHTML = myRace;
   document.getElementById("c").innerHTML = myClass;
@@ -25,16 +77,20 @@ function generate() {
   document.getElementById("w").innerHTML = myWeapons;
   document.getElementById("i").innerHTML = myInventory;
   document.getElementById("stats").innerHTML = "STRN = " + stats[0] + "<br>DEXT = " + stats[1] + "<br>INTL = " + stats[2] + "<br>MAGI = " + stats[3] + "<br>HLTH = " + stats[4] + "<br>ARMR = " + stats[5];
+
+  spells = ["Gravel Slap", "Ice Sweep", "Magic Teapot", "Cheese Shackles", "Pillow Armour", "Ember Bell"];
+  weapons = ["Club", "Bow", "Staff", "Sword", "Spear", "Knife"];
+  inventories = ["Rope", "Book", "Drugs", "Trinket", "Costume", "Companion", "Food", "Cards", "Poison", "Artifact", "Explosive", "Instrument"];
 }
 
 function rollArray(array) {
   return array[Math.floor(Math.random() * array.length)];
 }
 
-function rollDie(faces, isStat) {
+function rollDie(faces) {
   var temp = Math.floor(Math.random() * faces) + 1;
 
-  if (isStat && temp < 5) {
+  if (temp < 5) {
     temp = 5;
   }
 
