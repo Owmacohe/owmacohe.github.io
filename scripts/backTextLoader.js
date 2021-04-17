b79ed1985571403602d5f59c5aadf39ab0c8ccc9
OMCH <omch20012@gmail.com>
2021-04-16 20:41:44 -0400

    Added backtext animation. It's kinda client intensive, so may be removed

--- scripts/backTextLoader.js
+++ scripts/backTextLoader.js
@@ -1,9 +1,38 @@
+var backTextElem;
+
+var speed = 100;
+var colourDefault = 10000;
+var colourChance;
+
+var spikeChance = 300;
+var isSpiking = false;
+
 window.onload = function() {
-  var backTextElem = document.createElement("DIV");
+  backTextElem = document.createElement("DIV");
   document.body.appendChild(backTextElem);
   backTextElem.setAttribute("id", "backgroundText");
 
   backTextElem.innerHTML = getRandomCharacter(2.5 * window.innerWidth, "special");
+
+  colourChance = colourDefault;
+
+  if (speed != 0) {
+    var reloadBack = setInterval(function() {
+      backTextElem.innerHTML = getRandomCharacter(2.5 * window.innerWidth, "special");
+    }, speed);
+
+    var randomSpike = setInterval(function() {
+      if (isSpiking == false && Math.floor(Math.random() * spikeChance) == 0) {
+        isSpiking = true;
+        colourChance = colourDefault / 50;
+
+        var temp = setTimeout(function() {
+          colourChance = colourDefault;
+          isSpiking = false;
+        }, 1000);
+      }
+    }, speed);
+  }
 }
 
 function getRandomCharacter(iterations, type) {
@@ -28,7 +57,17 @@ function getRandomCharacter(iterations, type) {
   }
 
   for (var i = 0; i < iterations; i++) {
-    randOutput += characters[Math.floor(Math.random() * characters.length)];
+    var temp = Math.floor(Math.random() * colourChance);
+
+    if (temp >= 1 && temp < 15) {
+      randOutput += "<em>" + characters[Math.floor(Math.random() * characters.length)] + "</em>";
+    }
+    else if (temp == 0) {
+      randOutput += "<strong>" + characters[Math.floor(Math.random() * characters.length)] + "</strong>";
+    }
+    else {
+      randOutput += characters[Math.floor(Math.random() * characters.length)];
+    }
   }
 
   return randOutput;
