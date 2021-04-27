e0885b904c06dad7076004f2a6e6d3baf6219e8a
OMCH <omch20012@gmail.com>
2021-04-27 02:08:39 -0400

    Added carousel effect to image sea

--- scripts/sea.js
+++ scripts/sea.js
@@ -1,20 +1,71 @@
 window.onload = function() {
-  var imgCount = Math.floor(Math.random() * 3) + 3;
+  newWave(Math.floor(Math.random() * 4) + 3);
+}
+
+var signif = 0;
+
+function newWave(count) {
+  var imgCount = count;
   var imgRatio = 100 / imgCount;
-  var imgGray = (Math.floor(Math.random() * 15) + 85);
+  var imgGray = (Math.floor(Math.random() * 30) + 70);
 
   for (var i = 0; i < imgCount; i++) {
-    var newImg = document.createElement('IMG');
+    var newImg = document.createElement('img');
 
-    var temp = (230 * imgCount) + 10;
-    newImg.setAttribute('src', 'https://source.unsplash.com/random/400x' + temp + "?sig=" + i);
+    var tempX = Math.floor(window.innerWidth / 3.75);
+    var tempY = (230 * imgCount) + 10;
 
-    newImg.style.left = (imgRatio * i) + "%";
-    newImg.style.width = imgRatio + "%";
+    newImg.setAttribute('src', 'https://source.unsplash.com/random/' + tempX + 'x' + tempY + '?sig=' + signif);
+    signif++;
 
-    newImg.style.filter = "grayscale(" + imgGray + "%)";
-    newImg.style.zIndex = Math.floor(Math.random() * imgCount);
+    newImg.style.left = (imgRatio * i) + '%';
+    newImg.style.width = imgRatio + '%';
+    newImg.style.marginLeft = (Math.floor(Math.random() * 600) + 1400) + 'px';
+
+    newImg.style.filter = 'grayscale(' + imgGray + '%)';
 
     document.body.appendChild(newImg);
   }
 }
+
+var waveTime = 0;
+var waveSpeed;
+var isRushing = false;
+
+var waveCount = 0;
+
+var waves = setInterval(function() {
+  waveTime += 0.003;
+
+  if (waveTime >= 2) {
+    waveTime = 0;
+  }
+
+  var images = document.getElementsByTagName('img');
+
+  for (var i in images) {
+    var wavePace = Math.random() * 5;
+    waveSpeed = 100 * (wavePace * Math.cos(waveTime * Math.PI)) / 100 + wavePace;
+
+    try {
+      var temp = images[i].style.marginLeft.substring(0, images[i].style.marginLeft.length - 2);
+      temp -= waveSpeed;
+      images[i].style.marginLeft = temp + 'px';
+    }
+    catch (err) {
+      break;
+    }
+
+    var remaining = images[i].getBoundingClientRect().left;
+
+    if (waveCount < 30) {
+      var temp = (Math.floor(Math.random() * 4) + 3);
+      newWave(temp);
+      waveCount += temp;
+    }
+    else if (remaining <= -500) {
+      images[i].remove();
+      waveCount--;
+    }
+  }
+}, 1);
