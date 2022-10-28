6487902e86b03b568aa3fc9d4256c484e1c4f3b6
OMCH <44061383+Owmacohe@users.noreply.github.com>
2022-10-28 14:32:17 -0400

    Spelling mistake changes

--- ENGL398/script.js
+++ ENGL398/script.js
@@ -2,8 +2,8 @@ var captions = [
   [
     ['report_6/bart_1.jpg', 'Pretty god rays contrast the growing pollution above'],
     ['report_6/bart_2.jpg', 'The mist added by the Complementary Shaders between spires is really fantastic'],
-    ['report_6/bridge.jpg', 'Creature of use alternating redstone lights on the bridge by WonderAnchor\'s base'],
-    ['report_6/cacophany_1.jpg', 'Step one of the cacophany machine\'s construction'],
+    ['report_6/bridge.jpg', 'Creative of use alternating redstone lights on the bridge by WonderAnchor\'s base'],
+    ['report_6/cacophany_1.jpg', 'Step one of the cacophony machine\'s construction'],
     ['report_6/cacophany_2.jpg', 'Step two...'],
     ['report_6/cacophany_3.jpg', 'And step three. It makes a delightful racket'],
     ['report_6/cave.jpg', 'Wide open vista of an expansive 1.18 cave'],
@@ -25,21 +25,21 @@ var captions = [
     ['report_6/immersive_ice.jpg', 'A swift sunrise over a frozen lake (taken during the Immersive Space experience)'],
     ['report_6/immersive_nether_1.jpg', 'Glowstone sure shines bright with Complementary Shaders... (taken during the Immersive Space experience)'],
     ['report_6/immersive_nether_2.jpg', 'The warped forests are my favourite Nether biome. A great mix of comfort and uneasiness (taken during the Immersive Space experience)'],
-    ['report_6/immersive_pad.jpg', 'Light spilling dow from the glass roof above my base (taken during the Immersive Space experience)'],
+    ['report_6/immersive_pad.jpg', 'Light spilling down from the glass roof above my base (taken during the Immersive Space experience)'],
     ['report_6/immersive_planners_hub.jpg', 'A nice wide shot of the area surrounding Lunabunns\'s base (taken during the Immersive Space experience)'],
     ['report_6/immersive_radioactive_1.jpg', 'A high-quality shot of the \'radioactive\' monument we built to house the old image canvasses (taken during the Immersive Space experience)'],
-    ['report_6/immersive_radioactive_2.jpg', 'The same monement, this time from deep below (taken during the Immersive Space experience)'],
+    ['report_6/immersive_radioactive_2.jpg', 'The same monument, this time from deep below (taken during the Immersive Space experience)'],
     ['report_6/immersive_reef.jpg', 'One heck of a giant coral reef, with gorgeous reflections bouncing off the underneath of the waves (taken during the Immersive Space experience)'],
     ['report_6/immersive_teleplaza_1.jpg', 'The new teleportation hub (or Teleplaza) that Bl4ckbirds set up for us (taken during the Immersive Space experience)'],
     ['report_6/immersive_teleplaza_2.jpg', 'The hub, floating alone amid the stars in the Demon Realm (taken during the Immersive Space experience)'],
     ['report_6/immersive_wheat.jpg', 'With the extra-wide view, wheat fields seem to stretch on forever... (taken during the Immersive Space experience)'],
     ['report_6/jump.jpg', 'The Planner 2s demonstrating some very impressive bicycle tricks (right into my face)'],
     ['report_6/kelp.jpg', 'Kelp clusters as thick as the dark forests of the land'],
-    ['report_6/lilypads.jpg', 'A really pretty sunset vista reflecting off water and lilypads'],
+    ['report_6/lilypads.jpg', 'A really pretty sunset vista reflecting off water and lillypads'],
     ['report_6/seaside_village.jpg', 'A uniquely-generating structure: a seaside village'],
-    ['report_6/underwater.jpg', 'Yet more underwater reflections that alaways seem to blow my mind'],
+    ['report_6/underwater.jpg', 'Yet more underwater reflections that always seem to blow my mind'],
     ['report_6/waves.jpg', 'You\'d be forgiven for thinking that this isn\'t a Minecraft screenshot'],
-    ['report_6/whale.jpg', 'Another magestic animal from the Alex\'s Mobs mod, which continues to impress']
+    ['report_6/whale.jpg', 'Another majestic animal from the Alex\'s Mobs mod, which continues to impress']
   ]
 ];
 
