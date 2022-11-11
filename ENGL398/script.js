9bee4d324b25a489c3e30a443ed211e071dd7130
OMCH <44061383+Owmacohe@users.noreply.github.com>
2022-11-11 10:10:31 -0500

    Updated the report 7 images and added them to the script

--- ENGL398/script.js
+++ ENGL398/script.js
@@ -1,58 +1,85 @@
 var captions = [
   [
-    ['report_6/bart_1.jpg', 'Pretty god rays contrast the growing pollution above'],
-    ['report_6/bart_2.jpg', 'The mist added by the Complementary Shaders between spires is really fantastic'],
-    ['report_6/bridge.jpg', 'Creative of use alternating redstone lights on the bridge by WonderAnchor\'s base'],
-    ['report_6/cacophany_1.jpg', 'Step one of the cacophony machine\'s construction'],
-    ['report_6/cacophany_2.jpg', 'Step two...'],
-    ['report_6/cacophany_3.jpg', 'And step three. It makes a delightful racket'],
-    ['report_6/cave.jpg', 'Wide open vista of an expansive 1.18 cave'],
-    ['report_6/circle.jpg', 'From high up above, everything looks so much more... expansive'],
-    ['report_6/fortress.jpg', 'You can probably tell that I love taking fog screenshots. Also, green suits the Nether so well'],
-    ['report_6/gallery_1.jpg', 'The unaltered terrain below the Flâneur art galley before the slice of terrain got pulled up'],
-    ['report_6/gallery_2.jpg', 'The terrain extraction is progress as I flew through its beams of light'],
-    ['report_6/gallery_3.jpg', 'The newly-pulled island, ready for construction'],
-    ['report_6/gallery_4.jpg', 'The first iteration of the gallery, without proper lights or interior design'],
-    ['report_6/gallery_5.jpg', 'A later iteration...'],
-    ['report_6/gallery_interior_1.jpg', 'The image stacks, ready for adornment'],
-    ['report_6/gallery_interior_2.jpg', 'The upper level (you have no how idea how much gold I wasted on this build)'],
-    ['report_6/ice.jpg', 'The formations in the ice biome really are something else. This one reminds me of the CN tower'],
-    ['report_6/immersive_bomb.jpg', 'A poor villager town being utterly obliterated by Botania bombs (taken during the Immersive Space experience)'],
-    ['report_6/immersive_botania.jpg', 'The Botania island at work near spawn (taken during the Immersive Space experience)'],
-    ['report_6/immersive_end_1.jpg', 'One of the more unique End mobs (taken during the Immersive Space experience)'],
-    ['report_6/immersive_end_2.jpg', 'While purposeless, the End formations sure are breathtaking (taken during the Immersive Space experience)'],
-    ['report_6/immersive_end_3.jpg', 'The Ender Dragon flies by during our fight with it (taken during the Immersive Space experience)'],
-    ['report_6/immersive_ice.jpg', 'A swift sunrise over a frozen lake (taken during the Immersive Space experience)'],
-    ['report_6/immersive_nether_1.jpg', 'Glowstone sure shines bright with Complementary Shaders... (taken during the Immersive Space experience)'],
-    ['report_6/immersive_nether_2.jpg', 'The warped forests are my favourite Nether biome. A great mix of comfort and uneasiness (taken during the Immersive Space experience)'],
-    ['report_6/immersive_pad.jpg', 'Light spilling down from the glass roof above my base (taken during the Immersive Space experience)'],
-    ['report_6/immersive_planners_hub.jpg', 'A nice wide shot of the area surrounding Lunabunns\'s base (taken during the Immersive Space experience)'],
-    ['report_6/immersive_radioactive_1.jpg', 'A high-quality shot of the \'radioactive\' monument we built to house the old image canvasses (taken during the Immersive Space experience)'],
-    ['report_6/immersive_radioactive_2.jpg', 'The same monument, this time from deep below (taken during the Immersive Space experience)'],
-    ['report_6/immersive_reef.jpg', 'One heck of a giant coral reef, with gorgeous reflections bouncing off the underneath of the waves (taken during the Immersive Space experience)'],
-    ['report_6/immersive_teleplaza_1.jpg', 'The new teleportation hub (or Teleplaza) that Bl4ckbirds set up for us (taken during the Immersive Space experience)'],
-    ['report_6/immersive_teleplaza_2.jpg', 'The hub, floating alone amid the stars in the Demon Realm (taken during the Immersive Space experience)'],
-    ['report_6/immersive_wheat.jpg', 'With the extra-wide view, wheat fields seem to stretch on forever... (taken during the Immersive Space experience)'],
-    ['report_6/jump.jpg', 'The Planner 2s demonstrating some very impressive bicycle tricks (right into my face)'],
-    ['report_6/kelp.jpg', 'Kelp clusters as thick as the dark forests of the land'],
-    ['report_6/lilypads.jpg', 'A really pretty sunset vista reflecting off water and lillypads'],
-    ['report_6/seaside_village.jpg', 'A uniquely-generating structure: a seaside village'],
-    ['report_6/underwater.jpg', 'Yet more underwater reflections that always seem to blow my mind'],
-    ['report_6/waves.jpg', 'You\'d be forgiven for thinking that this isn\'t a Minecraft screenshot'],
-    ['report_6/whale.jpg', 'Another majestic animal from the Alex\'s Mobs mod, which continues to impress']
+    [['report_6/bart_1.jpg', 'report_6/bart_2.jpg'], 'Pretty god rays and Complementary Shaders mist contrast the growing pollution above'],
+    [['report_6/bridge.jpg'], 'Creative of use alternating redstone lights on the bridge by WonderAnchor\'s base'],
+    [['report_6/cacophany_1.jpg', 'report_6/cacophany_2.jpg', 'report_6/cacophany_3.jpg'], 'The cacophony machine\'s construction during various stages'],
+    [['report_6/cave.jpg'], 'Wide open vista of an expansive 1.18 cave'],
+    [['report_6/circle.jpg'], 'From high up above, everything looks so much more... expansive'],
+    [['report_6/fortress.jpg'], 'You can probably tell that I love taking fog screenshots. Also, green suits the Nether so well'],
+    [['report_6/gallery_1.jpg', 'report_6/gallery_2.jpg', 'report_6/gallery_3.jpg'], 'The unaltered terrain below the Flâneur art galley before, during, and after the slice of terrain got pulled up'],
+    [['report_6/gallery_4.jpg', 'report_6/gallery_5.jpg'], 'The first iteration of the gallery, with an unfinished interior'],
+    [['report_6/gallery_interior_1.jpg', 'report_6/gallery_interior_2.jpg'], 'The image stacks, ready for adornment (you have no how idea how much gold I wasted on this build)'],
+    [['report_6/ice.jpg'], 'The formations in the ice biome really are something else. This one reminds me of the CN tower'],
+    [['report_6/immersive_bomb.jpg'], 'A poor villager town being utterly obliterated by Botania bombs (taken during the Immersive Space experience)'],
+    [['report_6/immersive_botania.jpg'], 'The Botania island at work near spawn (taken during the Immersive Space experience)'],
+    [['report_6/immersive_end_1.jpg'], 'One of the more unique End mobs (taken during the Immersive Space experience)'],
+    [['report_6/immersive_end_2.jpg'], 'While purposeless, the End formations sure are breathtaking (taken during the Immersive Space experience)'],
+    [['report_6/immersive_end_3.jpg'], 'The Ender Dragon flies by during our fight with it (taken during the Immersive Space experience)'],
+    [['report_6/immersive_ice.jpg'], 'A swift sunrise over a frozen lake (taken during the Immersive Space experience)'],
+    [['report_6/immersive_nether_1.jpg'], 'Glowstone sure shines bright with Complementary Shaders... (taken during the Immersive Space experience)'],
+    [['report_6/immersive_nether_2.jpg'], 'The warped forests are my favourite Nether biome. A great mix of comfort and uneasiness (taken during the Immersive Space experience)'],
+    [['report_6/immersive_pad.jpg'], 'Light spilling down from the glass roof above my base (taken during the Immersive Space experience)'],
+    [['report_6/immersive_planners_hub.jpg'], 'A nice wide shot of the area surrounding Lunabunns\'s base (taken during the Immersive Space experience)'],
+    [['report_6/immersive_radioactive_1.jpg'], 'A high-quality shot of the \'radioactive\' monument we built to house the old image canvasses (taken during the Immersive Space experience)'],
+    [['report_6/immersive_radioactive_2.jpg'], 'The same monument, this time from deep below (taken during the Immersive Space experience)'],
+    [['report_6/immersive_reef.jpg'], 'One heck of a giant coral reef, with gorgeous reflections bouncing off the underneath of the waves (taken during the Immersive Space experience)'],
+    [['report_6/immersive_teleplaza_1.jpg'], 'The new teleportation hub (or Teleplaza) that Bl4ckbirds set up for us (taken during the Immersive Space experience)'],
+    [['report_6/immersive_teleplaza_2.jpg'], 'The hub, floating alone amid the stars in the Demon Realm (taken during the Immersive Space experience)'],
+    [['report_6/immersive_wheat.jpg'], 'With the extra-wide view, wheat fields seem to stretch on forever... (taken during the Immersive Space experience)'],
+    [['report_6/jump.jpg'], 'The Planner 2s demonstrating some very impressive bicycle tricks (right into my face)'],
+    [['report_6/kelp.jpg'], 'Kelp clusters as thick as the dark forests of the land'],
+    [['report_6/lilypads.jpg'], 'A really pretty sunset vista reflecting off water and lillypads'],
+    [['report_6/seaside_village.jpg'], 'A uniquely-generating structure: a seaside village'],
+    [['report_6/underwater.jpg'], 'Yet more underwater reflections that always seem to blow my mind'],
+    [['report_6/waves.jpg'], 'You\'d be forgiven for thinking that this isn\'t a Minecraft screenshot'],
+    [['report_6/whale.jpg'], 'Another majestic animal from the Alex\'s Mobs mod, which continues to impress']
+  ],
+  [
+    [['report_7/arcade.jpg', 'report_7/arcade_roof.jpg', 'report_7/arcade_chess.jpg'], 'The COM1 group are constructing an arcade which reminds me tremendously of The Crystal Palace'],
+    [['report_7/bamboo.jpg'], 'Jungle biomes can be breathtaking in their density'],
+    [['report_7/blossoms_1.jpg', 'report_7/blossoms_2.jpg', 'report_7/blossoms_3.jpg'], 'I\'ve been really obsessed with the look of the blue blossom trees lately'],
+    [['report_7/boats.jpg'], 'I have stumbled upon a secret temp base owned by <strong>WonderAnchor</strong>, out on the high seas...'],
+    [['report_7/bobby.jpg'], '<strong>TheCringeKid1</strong> stands next to COM1\'s new mascot: Bobby. Hi Bobby'],
+    [['report_7/cafe.jpg', 'report_7/cafe_emu.jpg'], 'The COM2 group has constructed a nice bright café near spawn. It seems to be staffed by birds, though they seem a wee bit aggressive...'],
+    [['report_7/glass_castle_1.jpg', 'report_7/glass_castle_2.jpg'], 'Another of <strong>WonderAnchor</strong>\'s secret creations floating out at sea'],
+    [['report_7/hub_pollution.jpg'], '<strong>Bl4ckbirds</strong> stands beside a sickly-yellow block of pollution in the Teleplaza'],
+    [['report_7/hub_ring_1.jpg', 'report_7/hub_ring_2.jpg', 'report_7/hub_reflections.jpg'], 'Some new features have been added around the Teleplaza, and they really help to give it that austere feel'],
+    [['report_7/ice_1.jpg', 'report_7/ice_2.jpg', 'report_7/ice_3.jpg', 'report_7/ice_4.jpg'], 'Complementary Shaders provide such amazing sky, reflection, and mist additions. I\'ll never, ever go back'],
+    [['report_7/mountains_1.jpg', 'report_7/mountains_2.jpg'], 'Cliifs! Cliffs! Cliffs!'],
+    [['report_7/nether_roof_1.jpg', 'report_7/nether_roof_2.jpg'], 'Getting really abstract up in the air above the Nether roof'],
+    [['report_7/POLM.jpg'], 'A few of the Polymaths, working hard on their train'],
+    [['report_7/red_cabin_1.jpg', 'report_7/red_cabin_2.jpg'], 'Some mysterious red cabins have begun to appear around the server, enticing players to take a peek inside...'],
+    [['report_7/rory_botania.jpg'], 'The Botania mod provides so many lovely colours!'],
+    [['report_7/snow_golem.jpg'], 'I\'ll let you try and figure out what\'s going on here'],
+    [['report_7/Super410_base.jpg'], '<strong>Super410</strong> has finished construction on a new Flâneur base! It looks great :)'],
+    [['report_7/The_9th_Doctor.jpg'], 'Professor <strong>The_9th_Doctor</strong> is always hard at work on some contraption during class time'],
+    [['report_7/theodorefox.jpg'], '<strong>theodorefox</strong>\'s skin is really eclectic. I love it'],
+    [['report_7/uranium.jpg'], 'Buried deep below <strong>WonderAnchor</strong>\'s base lies an airlock-guarded, uranium power system. It\'s incredibly ominous'],
+    [['report_7/vapidincarnate.jpg'], 'A relatively rare glimpse of <strong>vapidincarnate</strong>, our tireless behind-the-scenes technitian'],
+    [['report_7/void_wurm_fight.jpg', 'report_7/void_wurm_long.jpg', 'report_7/void_wurm_many.jpg', 'report_7/void_wurm_pillars.jpg'], 'A little while ago, <strong>Bl4ckbirds</strong> demonstrated what a Void Worm fight looks like. I nearly died a few times. It was great'],
+    [['report_7/water.jpg'], 'Obligatory underwater Complementary Shaders god ray shot'],
   ]
 ];
 
 window.onload = function() {
-  for (var i = 0; i < 1; i++) {
+  var start = 6;
+
+  for (var i = 0; i < captions.length; i++) {
     for (var j = 0; j < captions[i].length; j++) {
       var row = document.createElement('DIV');
       row.setAttribute('class', 'flex_row');
-      document.getElementById('report_' + (i+6)).appendChild(row);
+      document.getElementById('report_' + (i + start)).appendChild(row);
 
-      var img = document.createElement('IMG');
-      img.setAttribute('src', 'images/' + captions[i][j][0]);
-      row.appendChild(img);
+      var groupCount = captions[i][j][0].length;
+
+      for (var k = 0; k < groupCount; k++) {
+        var widthOffset = (groupCount != 4) ? 12 : 7;
+
+        var img = document.createElement('IMG');
+        img.setAttribute('src', 'images/' + captions[i][j][0][k]);
+        img.setAttribute('style', 'width: ' + (59 - (groupCount * widthOffset)) + 'vw;');
+        row.appendChild(img);
+      }
 
       var par = document.createElement('P');
       par.setAttribute('class', 'caption');
