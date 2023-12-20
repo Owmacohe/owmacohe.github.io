85802ea7308ef23afe8c0be41ee1a8d5307da5b5
OMCH <44061383+Owmacohe@users.noreply.github.com>
2023-12-20 17:20:12 -0500

    Finished adding component documentation for Descant

--- descant/components.js
+++ descant/components.js
@@ -1,133 +1,166 @@
 let components = [
     [
-        'ChangedChoice', 'float.PositiveInfinity', 'Choice',
-        'description',
+        'ChangedChoice', 'None', 'Choice',
+        'Changes the text of one of a ChoiceNode\'s choices if some condition is met.',
         [
-            'variable1', 'variable1 description',
-            'variable2', 'variable2 description',
-            'variable3', 'variable3 description',
-        ],
-        'disclaimers'
+            'ActorName', 'The name the actor who\'s variable is being checked',
+            'ChoiceNumber', 'The index of the choice being changed (base 1)',
+            'VariableType', 'The type of the variable being checked (e.g. <strong>Statistic</strong>, ' +
+                            '<strong>Topic</strong>, etc.)',
+            'VariableName', 'The name of the variable being checked',
+            'ComparisonType', 'The type of the comparison being performed against the variable',
+            'Comparison', 'The value to compare the variable against',
+            'ChangeTo', 'The text to change the choice to if the comparison succeeds',
+        ]
     ],
     [
-        'ChangedResponse', 'float.PositiveInfinity', 'Response',
-        'description',
+        'ChangedResponse', 'None', 'Response',
+        'Changes the text of a ResponseNode\'s response if some condition is met.',
         [
-            'variable1', 'variable1 description',
-            'variable2', 'variable2 description',
-            'variable3', 'variable3 description',
-        ],
-        'disclaimers'
+            'ActorName', 'The name the actor who\'s variable is being checking',
+            'VariableType', 'The type of the variable being checked (e.g. <strong>Statistic</strong>, ' +
+                            '<strong>Topic</strong>, etc.)',
+            'VariableName', 'The name of the variable being checked',
+            'ComparisonType', 'The type of the comparison being performed against the variable',
+            'Comparison', 'The value to compare the variable against',
+            'ChangeTo', 'The text to change the choice to if the comparison succeeds',
+        ]
     ],
     [
-        'Event', 'float.PositiveInfinity', 'Any',
-        'description',
+        'Event', 'None', 'Any',
+        'Calls a method from a script.',
         [
-            'variable1', 'variable1 description',
-            'variable2', 'variable2 description',
-            'variable3', 'variable3 description',
+            'ObjectTag', 'The tag of the <strong>GameObject</strong> on which the script is located (optional)',
+            'ScriptName', 'The name of the script on the <strong>GameObject</strong>',
+            'MethodName', 'The name of the method being called',
+            'Parameter', 'The parameter to pass to the method (optional)',
         ],
-        'disclaimers'
+        'If <strong>ObjectTag</strong> is empty, the first found script in the scene ' +
+        'with the given name is called instead.<br><br>If the <strong>ScriptName</strong> or <strong>MethodName</strong> ' +
+        'are empty, no method is called.<br><br>While <strong>Parameter</strong> is technically of type ' +
+        '<strong>string</strong>, <strong>Integers</strong> and <strong>Floats</strong> can also be written, and ' +
+        'will be parsed accordingly.<br><br>Currently, methods with more than one parameter cannot be called.'
     ],
     [
-        'Interruptable', '1', 'Response',
-        'description',
+        'Interruptable', 'None', 'Response',
+        'Allows for the dialogue to be abruptly stopped with a key or button press. Once this happens, a method ' +
+        'from a script can be called.',
         [
-            'variable1', 'variable1 description',
-            'variable2', 'variable2 description',
-            'variable3', 'variable3 description',
+            'KeyCode', 'The lowercase name of the key press to check for (e.g. "e", "space", etc.) (optional)',
+            'ButtonName', 'The name of the button press to check for (e.g. "Fire1", etc.) (optional)',
+            'ObjectTag', 'The tag of the <strong>GameObject</strong> on which the script is located (optional)',
+            'ScriptName', 'The name of the script on the <strong>GameObject</strong> (optional)',
+            'MethodName', 'The name of the method being called (optional)',
+            'Parameter', 'The parameter to pass to the method (optional)',
         ],
-        'disclaimers'
+        'If <strong>ObjectTag</strong> is empty, the first found script in the scene ' +
+        'with the given name is called instead.<br><br>If the <strong>ScriptName</strong> or <strong>MethodName</strong> ' +
+        'are empty, no method is called.<br><br>While <strong>Parameter</strong> is technically of type ' +
+        '<strong>string</strong>, <strong>Integers</strong> and <strong>Floats</strong> can also be written, and ' +
+        'will be parsed accordingly.<br><br>Currently, methods with more than one parameter cannot be called.'
     ],
     [
-        'LockedChoice', 'float.PositiveInfinity', 'Choice',
-        'description',
+        'LockedChoice', 'None', 'Choice',
+        'Removes one of a ChoiceNode\'s choices if some condition is met.',
         [
-            'variable1', 'variable1 description',
-            'variable2', 'variable2 description',
-            'variable3', 'variable3 description',
-        ],
-        'disclaimers'
+            'ActorName', 'The name the actor who\'s variable is being checked',
+            'ChoiceNumber', 'The index of the choice being removed (base 1)',
+            'VariableType', 'The type of the variable being checked (e.g. <strong>Statistic</strong>, ' +
+                            '<strong>Topic</strong>, etc.)',
+            'VariableName', 'The name of the variable being checked',
+            'ComparisonType', 'The type of the comparison being performed against the variable',
+            'Comparison', 'The value to compare the variable against',
+        ]
     ],
     [
-        'Log', 'float.PositiveInfinity', 'Any',
-        'description',
+        'Log', 'None', 'Any',
+        'Prints a <strong>Debug.Log</strong> to the console.',
         [
-            'variable1', 'variable1 description',
-            'variable2', 'variable2 description',
-            'variable3', 'variable3 description',
-        ],
-        'disclaimers'
+            'Message', 'The message to log',
+        ]
     ],
     [
-        'PortraitChange', 'float.PositiveInfinity', 'Any',
-        'description',
+        'PortraitChange', 'None', 'Any',
+        'Changes and actor\'s portrait.',
         [
-            'variable1', 'variable1 description',
-            'variable2', 'variable2 description',
-            'variable3', 'variable3 description',
+            'PlayerPortrait', 'Whether the portrait being changed is the player\'s or the NPC\'s.',
+            'ChangeType', 'The change being made to the portrait (set, enabled, or disabled)',
+            'PortraitName', 'The name of the new portrait to be set (optional)',
         ],
-        'disclaimers'
+        'For a portrait to be set, a <strong>Sprite</strong> with the corresponding name must be passed into the ' +
+        '<strong>DescantDialogueTrigger</strong>\'s <strong>portrait</strong> property.'
     ],
     [
         'RandomizedChoice', '1', 'Choice',
-        'description',
-        [
-            'variable1', 'variable1 description',
-            'variable2', 'variable2 description',
-            'variable3', 'variable3 description',
-        ],
-        'disclaimers'
+        'Randomizes/shuffles a ChoiceNode\'s choices.',
+        [],
+        'As a best practice, always make sure you only add this Component to a node <em>after</em> all other ' +
+        'Components have been added to that node (especially after ones that affect a specific choice).'
     ],
     [
-        'RelationshipChange', 'float.PositiveInfinity', 'Any',
-        'description',
+        'RelationshipChange', 'None', 'Any',
+        'Changes one of an actor\'s relationships.',
         [
-            'variable1', 'variable1 description',
-            'variable2', 'variable2 description',
-            'variable3', 'variable3 description',
+            'FirstActorName', 'The name of the actor who\'s relationship is being changed',
+            'SecondActorName', 'The name of the actor that the relationship corresponds to',
+            'OperationType', 'The change being made to the relationship (increase, decrease, or set)',
+            'OperationValue', 'The value to change the relationship by',
         ],
-        'disclaimers'
+        'Changing the first actor\'s relationship to the second will <em>not</em> change the second\'s relationship ' +
+        'to the first.'
     ],
     [
-        'StatisticChange', 'float.PositiveInfinity', 'Any',
-        'description',
+        'StatisticChange', 'None', 'Any',
+        'Changes an one of actor\'s statistics.',
         [
-            'variable1', 'variable1 description',
-            'variable2', 'variable2 description',
-            'variable3', 'variable3 description',
-        ],
-        'disclaimers'
+            'ActorName', 'The name the actor who\'s statistic is being changed',
+            'StatisticName', 'The name of the statistic being changed',
+            'OperationType', 'The change being made to the statistic (increase, decrease, or set)',
+            'OperationValue', 'The value to change the statistic by',
+        ]
     ],
     [
-        'StatisticReveal', 'float.PositiveInfinity', 'Any',
-        'description',
+        'StatisticReveal', 'None', 'Any',
+        'Calls a method from a script, passing it an actor\'s statistic (e.g. to display how an NPC\'s mood might ' +
+        'increase/decrease during a dialogue).',
         [
-            'variable1', 'variable1 description',
-            'variable2', 'variable2 description',
-            'variable3', 'variable3 description',
+            'ActorName', 'The name the actor who\'s variable is being revealed',
+            'StatisticName', 'The name of the statistic being revealed',
+            'ObjectTag', 'The tag of the <strong>GameObject</strong> on which the script is located (optional)',
+            'ScriptName', 'The name of the script on the <strong>GameObject</strong>',
+            'MethodName', 'The name of the method being called',
         ],
-        'disclaimers'
+        'If <strong>ObjectTag</strong> is empty, the first found script in the scene ' +
+        'with the given name is called instead.<br><br>If the <strong>ScriptName</strong> or <strong>MethodName</strong> ' +
+        'are empty, no method is called.<br><br>Currently, methods with more than one parameter cannot be called.'
     ],
     [
         'TimedChoice', '1', 'Choice',
-        'description',
+        'Imposes a time restriction for choosing a TimedChoice\'s choice. Each FixedUpdate frame that the time is ' +
+        'counting down, a method from a script can be called, passing the timer completion percentage (e.g. to ' +
+        'update a timer UI). When the time reaches 0, a choice is automatically made, and another method can be ' +
+        'called (e.g. to initiate a point penalty).',
         [
-            'variable1', 'variable1 description',
-            'variable2', 'variable2 description',
-            'variable3', 'variable3 description',
+            'Time', 'The amount of time the player has to choose (in seconds)',
+            'ChoiceToPick', 'The index of the choice to pick if the timer runs out (base 1)',
+            'ObjectTag', 'The tag of the <strong>GameObject</strong> on which the script is located (optional)',
+            'ScriptName', 'The name of the script on the <strong>GameObject</strong>',
+            'TimerMethodName', 'The name of the method called while the timer is counting down (optional)',
+            'FinishedMethodName', 'The name of the method called when the timer reaches 0 (optional)',
         ],
-        'disclaimers'
+        'If <strong>ObjectTag</strong> is empty, the first found script in the scene ' +
+        'with the given name is called instead.<br><br>If the <strong>ScriptName</strong> is empty, no method is ' +
+        'called. If <strong>TimerMethodName</strong> is empty, it isn\'t called. If <strong>FinishedMethodName</strong> ' +
+        'is empty, it isn\'t called.<br><br>Currently, methods with more than one parameter cannot be called.'
     ],
     [
-        'TopicChange', 'float.PositiveInfinity', 'Response',
-        'description',
+        'TopicChange', 'None', 'Response',
+        'Changes one of an actor\'s topics.',
         [
-            'variable1', 'variable1 description',
-            'variable2', 'variable2 description',
-            'variable3', 'variable3 description',
-        ],
-        'disclaimers'
+            'ActorName', 'The name the actor who\'s topic is being changed',
+            'TopicName', 'The name of the topic being changed',
+            'ChangeType', 'The change being made to the topic (add or remove)',
+        ]
     ],
 ];
 
@@ -146,10 +179,21 @@ window.onload = function () {
 
         component.innerHTML =
             '<h3 style="text-decoration: underline;"><strong>' + components[i][0] + '</strong></h3>' +
+            '<p style="margin-bottom: 30px">' + components[i][3] + '</p>' +
             '<p><strong>Max Quantity:</strong> ' + components[i][1] + '</p>' +
             '<p><strong>Node Type(s):</strong> ' + components[i][2] + '</p>' +
-            '<p>' + components[i][3] + '</p>' +
-            variables +
-            '<p>' + components[i][5] + '</p>';
+            (components[i][4].length === 0 ? '' : ('<p style="margin-top: 30px">Variables:</p>' + variables)) +
+            (components[i].length < 6 ? ''
+                : '<p style="margin-top: 30px">Disclaimers:</p><p class="important">' + components[i][5] + '</p>');
+    }
+
+    let anchors = document.querySelectorAll('a');
+
+    for (const j in anchors) {
+        try {
+            anchors[j].setAttribute('rel', 'noreferrer noopener');
+            anchors[j].setAttribute('target', '_blank');
+        }
+        catch { }
     }
 };
