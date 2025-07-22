f4c27637fe71cf8cbe8340ab4f332db84faeafae
Owmacohe <omch@pm.me>
2025-07-22 19:05:55 -0400

    Updated portfolio page & resume

--- cards.js
+++ cards.js
@@ -1,14 +1,35 @@
 let cards = [
     [
+        'Bonaparte: A Mechanized Revolution',
+        'Unity 3D, C#',
+        'https://bonapartegame.com',
+        'https://store.steampowered.com/app/2560340/Bonaparte__A_Mechanized_Revolution',
+        '',
+        '',
+        '1789, Paris. Take Napoleon’s place in an alternate history as Céline or César Bonaparte in this ideology driven, turn-based strategy journey. Defend, reform or oppose the monarchy and write your own alternative history by combining cunning political skills and tactical military prowess.',
+        'As a generalist programmer at <a href="https://www.linkedin.com/company/studio-imugi">Studio Imugi</a>, I co-design and implement game mechanics, as well as integrate UI designs, Wwise audio, Spine animations, and much more. I also create in-house development tools, and fix bugs. During my time working on Bonaparte I have become the project\'s key developer, bridging the gaps between tactical and strategic layers of the game, and contributing a large majority of commits and improvements.\n\nFrom dialogue system implementations, to custom event scheduling, to pathfinding optimizations, my Unity experience is put to work in this multi-layered strategy-tactics hybrid game.\n\nBonaparte: A Mechanized Revolution released in Early Access in May 2025, and will release at version 1.0 in Fall 2025.',
+        'bonaparte.jpg'
+    ],
+    [
         'My Friend Aki',
         'Unity 3D, C#, Blender, Substance Painter',
-        'https://myfriendaki.com',
+        'myfriendaki',
         'https://store.steampowered.com/app/2584180/My_Friend_Aki',
         'https://duck-duck-juice.itch.io/my-friend-aki',
         '',
         'A first-person 3D mall exploration and puzzle solving game with a good dose of story and horror. Use improvised tools to solve the environmental puzzles, discover the gloomy husk of an abandoned mall, and use the dynamic lighting system to bring areas of the game back to life.',
         'Over the game\'s 6 month development, I acted as production manager, programming and puzzle implementation lead, and 3D modeller alongside my other team members. I worked using the <a href="https://github.com/StephanieRct/NiEngine">NiEngine state engine</a> to implement virtually all interactions within the game, from menu screens, to puzzles, and even for the credits. I also put the game\'s <a href="https://myfriendaki.com">website</a> together.\n\nMy Friend Aki released on <a href="https://store.steampowered.com/app/2584180/My_Friend_Aki">Steam</a> and <a href="https://duck-duck-juice.itch.io/my-friend-aki">Itch</a> on November 9th 2023, and has since surpassed 190 downloads and over 1,000 impressions. My team and I are overjoyed and are extremely thankful for everyone who wishlisted, downloaded, and played the game.',
         'my_friend_aki.png'
+    ],
+    [
+        'Descant',
+        'C#',
+        'https://omch.tech/descant',
+        '', '',
+        'https://assetstore.unity.com/packages/tools/visual-scripting/descant-273262',
+        'Descant is a Unity dialogue system plugin. It aims to hit the sweet spot between quality UI, powerful features, and easy-to-lean functionality, while also adding optional \'dialogue enhancements\' that introduce features to break away from underwhelming trends seen in many interactive fiction games. These node enhancements act similar to Unity’s standard GameObject Component system.',
+        'The Unity Asset Store is chock full of many types of dialogue plugins, ranging from feature-rich, to ultra-minimalist, to downright bad. Descant\'s modular approach is so-far not explored in the world of Unity dialogue systems. The project will be free <i>(and hopefully collaborative open-source)</i> forever.\n\n\n',
+        'descant.png'
     ],
     [
         'UNDERSCORE',
@@ -21,14 +42,24 @@ let cards = [
         'underscore.jpg'
     ],
     [
-        'Descant',
+        'Resonant',
         'C#',
-        'https://omch.tech/descant',
+        'https://github.com/owmacohe/resonant',
         '', '',
-        'https://assetstore.unity.com/packages/tools/visual-scripting/descant-273262',
-        'Descant is a Unity dialogue system plugin. It aims to hit the sweet spot between quality UI, powerful features, and easy-to-lean functionality, while also adding optional \'dialogue enhancements\' that introduce features to break away from underwhelming trends seen in many interactive fiction games. These node enhancements act similar to Unity’s standard GameObject Component system.',
-        'The Unity Asset Store is chock full of many types of dialogue plugins, ranging from feature-rich, to ultra-minimalist, to downright bad. Descant\'s modular approach is so-far not explored in the world of Unity dialogue systems. The project will be free <i>(and hopefully collaborative open-source)</i> forever.',
-        'descant.png'
+        'https://assetstore.unity.com/packages/tools/audio/resonant-316689',
+        'Resonant is a powerful modular audio manager and sound randomizer. Leveraging capabilities for randomized, looping sound clips, randomized volume and pitch modulation, and customizable audio events/reactions, Resonant is extendable and multi-purposed. Though it\'s powerful trigger/reaction system, design audio events and reactions to real-time code.',
+        '\n\n\n\n\n',
+        'resonant.jpg'
+    ],
+    [
+        'Concordant',
+        'C#',
+        'https://github.com/owmacohe/concordant',
+        '', '',
+        'https://assetstore.unity.com/packages/tools/localization/concordant-315363',
+        'Concordant is a simple yet powerful text localization manager for Unity. Create, modify, export, and import databases of terms, sentences, or any other localizable text into any system language. Designed in an effort to make a cheap and easy-to-use alternative to many of the paid localization managers on the Unity Asset Store.',
+        'In-game dialogue updates immediately, as soon as the current language is changed, for immediate hot-swapping results. Concordant aims to capture the core principles and features of larger localization managers, without succumbing to feature-bloat or indecipherable UIs.\n\n\n\n',
+        'concordant.jpg'
     ],
     [
         'Something Quiet',
@@ -37,7 +68,7 @@ let cards = [
         'https://omch.itch.io/something-quiet',
         '',
         'A meditative and single-minded experience crafted to explore feeling and atmosphere through environment, sound design, poetry, and endless repetition. Taking advantage of advanced rendering techniques and fog shaders in Unity 6 and the Universal Render Pipeline, the game is both technical and whimsical.',
-        '\n\n\n\n',
+        '\n\n\n\n\n',
         'something_quiet.png'
     ],
     [
@@ -47,27 +78,8 @@ let cards = [
         'https://omch.itch.io/last-breath',
         '',
         'A short, narrative-focused resource-management game inspired by real-Time Strategy (RTS) classics such as Stellaris or Age of Empires. Traditional RTS games focus sp heavily on gameplay that story is often left behind, and Last Breath Of A New Empire attempts to flip that script. I developed a custom dialogue manager for the game, which would go on to inspire my work on above Descant Dialogue Manager.',
-        '\n\n\n\n',
+        '\n\n\n\n\n',
         'lboane.jpg'
-    ],
-    [
-        'Pantry',
-        'Javascript',
-        'https://github.com/owmacohe/pantry',
-        '', '', '',
-        'A tag-based wiki system is inspired by <a href="https://tiddlywiki.com">TiddlyWiki</a>. Operating purely through Javascript magic, it turns MarkDown files into hypertext articles, which can be tagged, linked to, and generated as dynamic lists. It requires no programming knowledge, save for some understanding of the system\'s modified MarkDown language (see below for more), and the ability to edit <b>.md</b> and <b>.txt</b> files.',
-        '\n\n\n\n',
-        'pantry.png'
-    ],
-    [
-        'Cultus Dextra',
-        'Unity 2D, C#',
-        '', '',
-        'https://omch.itch.io/cultus-dextra',
-        '',
-        'A resource-management game about culling the weak from your fanatical cult, with inspirations taken from roguelikes and rhythm games. Through story exposition, haunting graphics, and simple mechanics, the game aims to deliver visceral moments, tension, and damned-if-you-do damned-if-you-don\'t gameplay.',
-        '\n\n\n\n',
-        'cultus_dextra.png'
     ]
 ];
 
