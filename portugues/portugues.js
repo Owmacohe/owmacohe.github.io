6a345ff1ed8f189dd180254fb33523f487517f80
Owmacohe <omch@pm.me>
2026-07-13 20:39:13 -0400

    Fixed Portuguese page visuals

--- portugues/portugues.js
+++ portugues/portugues.js
@@ -1,7 +1,12 @@
+let sectionTitle;
 let title;
 let description;
+let answerTitle;
 let answer;
 let search;
+
+let reloadButton;
+let answerButton;
 
 let lists = [];
 let data;
@@ -10,15 +15,20 @@ let section = 0;
 
 window.onload = () => {
     readJsonFile('portugues.json').then(data => {
-        lists[0] = data.queueCards;
-        lists[1] = data.usefulPhrases;
-        lists[2] = data.verbs;
-        lists[3] = data.dictionary;
+        lists[0] = ["Queue cards", data.queueCards];
+        lists[1] = ["Useful phrases", data.usefulPhrases];
+        lists[2] = ["Verbs", data.verbs];
+        lists[3] = ["Dictionary", data.dictionary];
 
+        sectionTitle = document.getElementById('sectionTitle');
         title = document.getElementById('title');
         description = document.getElementById('description');
+        answerTitle = document.getElementById('answerTitle');
         answer = document.getElementById('answer');
         search = document.getElementById('search');
+
+        reloadButton = document.getElementById('reloadButton');
+        answerButton = document.getElementById('answerButton');
 
         search.addEventListener('input', searchDictionary);
 
@@ -29,14 +39,21 @@ window.onload = () => {
 function reload(newSection = -1) {
     if (newSection >= 0) section = newSection;
 
-    let list = lists[section];
+    let list = lists[section][1];
 
     if (list === undefined) return null;
 
-    let selected = list[Math.floor(Math.random() * list.length)];
-    let swap = Math.floor(Math.random() * 2) === 0;
+    if (list.length === 0) window.location.reload();
+    else {
+        let index = Math.floor(Math.random() * list.length);
+        let selected = list[index];
+        let swap = Math.floor(Math.random() * 2) === 0;
 
-    setData(getSelectedData(selected, swap));
+        sectionTitle.innerHTML = lists[section][0];
+
+        setData(getSelectedData(selected, swap));
+        list.splice(index, 1);
+    }
 }
 
 function setData(data) {
@@ -44,7 +61,19 @@ function setData(data) {
 
     title.innerHTML = data === null ? "..." : data.title;
     description.innerHTML = data === null ? "..." : data.description;
-    answer.innerHTML = '...';
+
+    var answerHidden = '';
+
+    for (let i in data.answer.split('\n'))
+        answerHidden += '...\n';
+
+    answerTitle.style.display = section === 0 || section === 2 ? 'flex' : 'none';
+    description.style.display = section === 0 || section === 2 ? 'flex' : 'none';
+
+    answer.innerHTML = answerHidden;
+    answer.style.columnCount = section === 2 ? '3' : 'auto';
+
+    answerButton.style.display = 'flex';
 
     search.style.display = section === 3 ? "flex" : "none";
 }
@@ -85,7 +114,8 @@ function getSelectedData(selected, swap = false) {
 }
 
 function showAnswer() {
-    answer.innerHTML = this.data === null ? "..." : this.data.answer;
+    answer.innerHTML = this.data.answer;
+    answerButton.style.display = 'none';
 }
 
 function searchDictionary(event) {
@@ -95,7 +125,7 @@ function searchDictionary(event) {
 
     if (name === "") return;
 
-    let dictionary = lists[3];
+    let dictionary = lists[3][1];
 
     for (let i = 0; i < dictionary.length; i++) {
         if (dictionary[i].word.toLowerCase().includes(name) || dictionary[i].answer.toLowerCase().includes(name)) {
